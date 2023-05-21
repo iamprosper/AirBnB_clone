8252d10eb41c7c80503bfccc21205e09f3f129af
iamprosper <thinkpadprosper@gmail.com>
2023-05-21 11:43:11 +0000

    Base model with tests

---
+++ models/base_model.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Base module module file"""
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+    """Base class"""
+    def __init__(self):
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(
+                self.__class__.__name__,
+                self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        self.__dict__["__class__"] = self.__class__.__name__
+        self.created_at = self.created_at.isoformat()
+        self.updated_at = self.updated_at.isoformat()
+        return self.__dict__
